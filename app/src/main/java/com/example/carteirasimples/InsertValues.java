d3e3139cedfbee8eaa12ec5ac94398b7bfde34dc
Gabriel Almeida <gabrielcrta@gmail.com>
2016-12-15 16:48:12 -0200

    Add CarteiraSimples project, created 14/12/16

---
+++ app/src/main/java/com/example/carteirasimples/InsertValues.java
@@ -0,0 +1,73 @@
+package com.example.carteirasimples;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+public class InsertValues extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    //public List<String> savedValues;
+    private static String savedValues = "";
+    private boolean sign = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_value);
+        RadioButton radioPositive = (RadioButton) findViewById(R.id.is_income);
+        radioPositive.setChecked(true);
+    }
+
+    public void changeToWalletView(View view) {
+        Intent walletViewIntent = new Intent(this, ViewValues.class);
+        walletViewIntent.putExtra(EXTRA_MESSAGE, savedValues);
+        startActivity(walletViewIntent);
+    }
+
+    /* called after  */
+    public void saveValue(View view){
+        String signal = "";
+        TextView showValue = (TextView) findViewById(R.id.show_value_added);
+        EditText valueToAdd = (EditText) findViewById(R.id.value_to_add);
+
+        if (!sign) {
+            signal = "-";
+        }
+        String valueAdded = valueToAdd.getText().toString();
+
+        if (!valueAdded.isEmpty() && !valueAdded.equals("0")) {
+            savedValues += signal+valueAdded+"|";
+            String message = getString(R.string.added) +" " + signal + valueAdded;
+            showValue.setText(message);
+        }
+        else {
+            showValue.setText(R.string.invalid_input);
+        }
+
+        //savedValues.add(valueAdded);
+
+        valueToAdd.setText("");
+    }
+
+    public void set_sign(View view){
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.is_income:
+                if (checked) {
+                    sign = true;
+                }
+                break;
+            case R.id.is_outcome:
+                if (checked) {
+                    sign = false;
+                }
+                break;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/carteirasimples/ViewValues.java
@@ -0,0 +1,29 @@
+package com.example.carteirasimples;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.regex.Pattern;
+
+public class ViewValues extends AppCompatActivity {
+    ListView walletListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_values);
+
+        Intent fromInsertValues = getIntent();
+        String message = fromInsertValues.getStringExtra(InsertValues.EXTRA_MESSAGE);
+        String data[] = message.split(Pattern.quote("|"));
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
+
+        walletListView = (ListView) findViewById(R.id.wallet_list_view);
+        walletListView.setAdapter(arrayAdapter);
+    }
+
+}
